3c76fa208b8750628f11267be07568b6ef9f3538
DanilSamkov <danilsamkov04@gmail.com>
2026-03-23 22:33:48 +0200

    Add JUnit tests

---
+++ src/test/java/org/example/ClothesTest.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ClothesTest {
+
+    @Test
+    void succesfullClothCreation(){
+
+        Clothes item = new Clothes("Футболка", "XXL", 250.50, "Чорний");
+
+        assertNotNull(item);
+
+        assertEquals("Футболка", item.getName());
+        assertEquals("XXL", item.getSize());
+        assertEquals("Чорний", item.getColor());
+        assertEquals(250.50, item.getPrice());
+
+        item.setPrice(450.0);
+        item.setSize("XL");
+
+        assertEquals(450.0, item.getPrice());
+        assertEquals("XL", item.getSize());
+    }
+
+    @Test
+    void setIllegalExceptionsTest(){
+        Clothes item = new Clothes("Джинси", "S", 900.25, "Синій");
+
+        assertThrows(IllegalArgumentException.class,()->{item.setName("");});
+        assertThrows(IllegalArgumentException.class,()->{item.setColor("");});
+        assertThrows(IllegalArgumentException.class,()->{item.setSize("");});
+        assertThrows(IllegalArgumentException.class,()->{item.setSize("B");});
+        assertThrows(IllegalArgumentException.class,()->{item.setPrice(-1);});
+    }
+}
